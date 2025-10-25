3557a24baf486eb2728ce1fff33a7f16f142d4d5
Milad Shukri <50368318+MiladShukri@users.noreply.github.com>
2025-10-25 13:28:19 +0100

    set bg to light blue

--- components/our-brands-section.tsx
+++ components/our-brands-section.tsx
@@ -115,7 +115,7 @@ export default function OurBrandsSection() {
     <section
       ref={sectionRef}
       id="brands"
-      className="relative h-screen overflow-hidden"
+      className="relative h-screen overflow-hidden bg-[#f6fdfe]"
     >
       {/* Section Header */}
       <div className="absolute top-16 left-0 right-0 z-50 px-4 md:px-8 pt-8">
